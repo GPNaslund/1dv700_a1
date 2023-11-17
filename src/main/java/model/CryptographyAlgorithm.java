5a4b315b9e4e3998021c0b9772e06d67433f796d
GPNaslund <135686203+GPNaslund@users.noreply.github.com>
2023-11-17 11:01:18 +0100

    Removes cipher application

--- src/main/java/model/CryptographyAlgorithm.java
+++
@@ -1,9 +0,0 @@
-package model;
-
-/**
- * Interface for the different cryptography algorithms.
- */
-public interface CryptographyAlgorithm {
-    String encrypt(String text, CryptographyKey key);
-    String decrypt(String text, CryptographyKey key);
-}
